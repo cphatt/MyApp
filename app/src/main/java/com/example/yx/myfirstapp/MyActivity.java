38a598d5312906b2844216902fbedc39eb846714
cphatt <850601130@qq.com>
2018-05-24 11:19:49 +0800

    first commit

---
+++ app/src/main/java/com/example/yx/myfirstapp/MyActivity.java
@@ -0,0 +1,110 @@
+package com.example.yx.myfirstapp;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.util.List;
+
+
+public class MyActivity extends AppCompatActivity {
+    public final static String ProssName = "com.example.yx.test";
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case 0:
+                    // 移除所有的msg.what为0等消息，保证只有一个循环消息队列再跑
+                    handler.removeMessages(0);
+                    // app的功能逻辑处理
+                    if(isProessRunning(getBaseContext(),ProssName)){
+                        //Launcher
+                    }else{
+                        //Close
+                    }
+                    // 再次发出msg，循环更新
+                    handler.sendEmptyMessageDelayed(0, 1000*60 );
+                    break;
+
+                case 1:
+                    // 直接移除，定时器停止
+                    handler.removeMessages(0);
+                    break;
+
+                default:
+                    break;
+            }
+        };
+    };
+
+    /**
+     * 进程运行状态
+     * @param context
+     * @param proessName    进程名
+     * @return
+     */
+    public static boolean isProessRunning(Context context, String proessName) {
+
+        boolean isRunning = false;
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+
+        List<ActivityManager.RunningAppProcessInfo> lists = am.getRunningAppProcesses();
+        for(ActivityManager.RunningAppProcessInfo info : lists){
+            if(info.processName.equals(proessName)){
+                //Log.i("Service2进程", ""+info.processName);
+                isRunning = true;
+            }
+        }
+
+        return isRunning;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * 开启定时器
+     * @param view
+     */
+    public void startThread(View view){
+        handler.sendEmptyMessage(0);
+    }
+
+    /**
+     * 关闭定时器
+     * @param view
+     */
+    public void stopThread(View view){
+        handler.sendEmptyMessage(1);
+    }
+}
